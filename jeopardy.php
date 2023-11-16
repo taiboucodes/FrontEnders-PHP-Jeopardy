ba30e1d3d3ef54fe226f51ec88d960acdfce30e4
iamtai <taiboubalde101@gmail.com>
2023-11-15 21:35:20 -0500

    updated jeopardy file

--- jeopardy.php
+++ jeopardy.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,11 +6,53 @@
     <title>Jeopardy</title>
 </head>
 <body>
-
     
     <?php
+    function userExists($username, $password) {
+        $users = file('users.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($users as $user) {
+            list($name, $storedUsername, $storedPassword) = explode(',', $user);
+            if ($username == $storedUsername && password_verify($password, trim($storedPassword))) {
+                return $name;
+            }
+        }
+        return false;
+    }
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
 
-        echo "<h1> Welcome ". $_POST["name"]."</h1>";
+         $name = userExists($username, $password); //return name 
+        if ($name) {
+            echo "Login successful. Welcome, $name!"; //takes user to game page
+        } else {
+            echo "Login failed. Invalid username or password.";
+        }
+    }
+
+    include 'questions.php';
+
+    echo '<table border=\'1\'>';
+    echo '<tr>';
+    foreach ($questions as $category => $groupedQuestions) {
+        echo '<th>' . $category . '</th>';
+    
+    }
+    echo "</tr>";
+
+            
+    for ($i = 0; $i < 5; $i++) {
+        echo"<tr>";
+
+        foreach ($questions as $category => $groupedQuestions) {
+                $question = $groupedQuestions[$i];
+                echo "<td><a href=\"display.php?question=" . htmlentities(json_encode($question)) . "\">" . $question["points"] . "</a></td>";
+            }
+        echo '</tr>';
+        }
+    echo "</table>"
+
+
 
     ?>
 
