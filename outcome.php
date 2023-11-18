f10734e135bed5c8003303cad294450581b5fc01
Sadekin Hasnath <129838189+shasnath0830@users.noreply.github.com>
2023-11-18 02:55:19 -0500

    Add files via upload

--- outcome.php
+++ outcome.php
@@ -1,41 +1,44 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Outcome</title>
+    <link rel="stylesheet" href="outcome.css">
+</head>
+<body>
 
-session_start();
+    <?php
+    
+    session_start();
 
-// Initialize score if not already set
-if (!isset($_SESSION['score'])) {
-    $_SESSION['score'] = 0;
-}
-
-
+    // Initialize score if not already set
+    if (!isset($_SESSION['score'])) {
+        $_SESSION['score'] = 0;
+    }
     $selectedAnswer = $_POST["answerOption"];
     $correctAnswer = json_decode(html_entity_decode($_POST['answer']), true);
     $points = json_decode(html_entity_decode($_POST['points']), true);
 
-
-
     if ($selectedAnswer == $correctAnswer) {
-        print("right answer!");
-        $_SESSION['score'] += $points;
-    print("you have " . $points . " points");
+        $_SESSION['score'] += $points; // Update score before creating the message
+        $message = "Correct answer! You have " . $_SESSION['score'] . " points."; // Now the updated score will be displayed
+        $color = "#5cb85c"; // Green for correct answers
+    } else {
+        $message = "Incorrect! The correct answer choice was " . $correctAnswer . ".";
+        $color = "#d9534f"; // Red for incorrect answers
+        $_SESSION['score'] -= $points;
     }
-    else{
-        print("wrong answer! the correct answer was " . $correctAnswer);
+
+    echo "<div id='outcome-message' style='color: {$color};'>";
+    echo "<h1>{$message}</h1>";
+    if ($selectedAnswer == $correctAnswer) {
+        echo "<img src='firework.gif' alt='Correct Answer'/>";
     }
+    echo "<a href='jeopardy.php' id='return-button'>Return to Game</a>";
+    echo "</div>";
 
+    ?>
 
-//     $correctAnswer = $answer;
-//     $points = $_POST["points"];
-
-//      // Compare the selected answer ID with the correct answer ID
-//      if ($selectedAnswer === $correctAnswer) {
-//         echo "Correct! You've earned $points points.";
-//         $_SESSION['score'] += $points;
-//     } else {
-//         echo "Incorrect! The correct answer was option " . $correctAnswer . ".";
-//     }
-
-//     echo "<br>Your current score is: " . $_SESSION['score'];
-//     echo "<br><a href='jeopardy.php'>Back to the game board</a>";
-
-?>
+</body>
+</html>
