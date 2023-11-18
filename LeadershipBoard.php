de8b1e6ce97bfba1f61c6a39e6b4f5d251651c7b
Phillip DelRossi <101755335+filupdilrosi@users.noreply.github.com>
2023-11-18 11:16:44 -0500

    Update LeadershipBoard.php
    nav bar update

--- LeadershipBoard.php
+++ LeadershipBoard.php
@@ -8,7 +8,33 @@
 
 </head>
 <body>
-    
+	<div class="navbar">
+        <?php
+            session_start();
+
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $username = $_POST["username"];
+                $password = $_POST["password"];
+
+                $name = userExists($username, $password); //return name 
+                if ($name) {
+                    $_SESSION['username'] = $username; // Save the username in the session
+                    $_SESSION['name'] = $name; // Save the name in the session
+                }
+            }
+
+            // Display the user's name if logged in
+            if (isset($_SESSION['name'])) {
+                echo '<div class="user-container">Welcome, ' . $_SESSION['name'] . '</div>';
+            }
+			// Display the user's current score
+            if (isset($_SESSION['score'])) {
+                echo '<div class="user-score">Score: ' . $_SESSION['score'] . '</div>';
+            }
+        ?>
+        <a href="LeadershipBoard.php" class="leaderboard-container">Leaderboard</a>
+        <a href="logout.php" class="logout-button">Log Out</a>
+    </div>
     <?php
    //The function read is used to read the score 
 
